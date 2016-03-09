e403a75bcb9d9f8dee67b215a86555e5cbf5658e
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2016-03-09 11:38:13 +0300

    create server for 1.6-step6

---
+++ module1/1.6-step6/server.py
@@ -0,0 +1,13 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.bind(('0.0.0.0', 2222))
+s.listen(1)
+while True:
+    conn, addr = s.accept()
+    while True:
+        data = conn.recv(1024)
+        if data.find('close') != -1:
+            break
+        conn.send(data)
+    conn.close()
+
